760e66da8f3cf7d25b48ec9a0345f91cad58bd80
Fernando Marichal <fernando.marichal@automattic.com>
2024-02-16 17:28:01 -0300

    Visibility for variations

--- packages/js/product-editor/src/components/header/header.tsx
+++ packages/js/product-editor/src/components/header/header.tsx
@@ -73,6 +73,12 @@ export function Header( {
 		Product[ 'catalog_visibility' ]
 	>( 'postType', productType, 'catalog_visibility' );
 
+	const [ productStatus ] = useEntityProp< string >(
+		'postType',
+		productType,
+		'status'
+	);
+
 	const sidebarWidth = useAdminSidebarWidth();
 
 	useEffect( () => {
@@ -107,12 +113,12 @@ export function Header( {
 	const isDraft =
 		productType === 'product'
 			? lastPersistedProduct?.status === 'draft'
-			: true;
+			: false;
 
 	const isScheduled =
 		productType === 'product'
 			? lastPersistedProduct?.status === 'future'
-			: true;
+			: false;
 
 	const isHeaderImageVisible =
 		( ! isVariation &&
@@ -140,7 +146,13 @@ export function Header( {
 				/>
 			);
 		}
-		if ( ! isScheduled && catalogVisibility !== 'visible' ) {
+		if (
+			( productType === 'product' &&
+				! isScheduled &&
+				catalogVisibility !== 'visible' ) ||
+			( productType === 'product_variation' &&
+				productStatus === 'private' )
+		) {
 			tags.push(
 				<Tag
 					key={ 'hidden-tag' }
